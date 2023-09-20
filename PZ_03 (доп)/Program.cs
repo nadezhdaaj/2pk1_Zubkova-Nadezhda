ea689b79bdc766b0fc97a88abd2b012fb3f1138a
admin <admin@EVE_15_P417>
2023-09-20 23:41:15 +0500

    добавлена 4 практика

---
+++ PZ_03 (доп)/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PZ_03__доп_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //использовать бесконечный цикл. Пользователь вводит название услуги ему выдается цена данной услуги. 
+            // при пустом значении цикл завершается. В конце вывести общую сумму услуг
+            Console.WriteLine("Введите название услуги");
+            string nameServer = Console.ReadLine();
+            for(; ;)
+            {
+                Console.WriteLine("Всё для вас, всё бесплатно");
+            }
+        }
+    }
+}
---
+++ PZ_04/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Specialized;
+
+namespace PZ_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
+
+// Задача 1
+/* Console.WriteLine("Вывод диапазона чисел");
+//объявление начальной, шаговой и конечной переменной
+int start = 20;
+int step = 2;
+int end = 90;
+for (int i = start; i<=end; i+=step) //начало цикла
+{ 
+    Console.WriteLine(i);
+} */
+
+
+
+// Задача 2
+/* Console.WriteLine("Вывод символов в алфавитном порядке. P.S. английский алфавит");
+
+           for(char  c = 'H'; c <= 'M'; c++)
+           {
+               Console.Write(c);
+           } */
+
+
+
+// Задача 3
+/* Console.WriteLine("Вывод посимвольных знаков");
+int l = 6;
+int k = 6;
+for (int f = 0; f < l; f++)
+{
+    for (int d = 0; d < k; d++)
+    {
+        Console.Write('#');
+    }
+    Console.WriteLine();
+} */
+
+
+
+// Задача 4
+/*Console.WriteLine("Числа в диапозоне значений кратные 2");
+int start = -500;
+int end = -200;
+for (int i = start; i <= end; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    } */
+
+
+
+// Задача 5
+/*Console.WriteLine("Итерация двух переменных");
+int i, j;
+for (i = 4, j = 40; Math.Abs(i - j) >= 13; i++, j--)
+{
+    Console.WriteLine(" i = " + i + " j = " + j);
+} */
