7b05e4697c59c3360494aa35081b0011489d254b
admin <admin@EVE_15_P417>
2023-09-22 22:25:33 +0500

    добавлена 5 практика

--- PZ_03 (доп)/Program.cs
+++ PZ_03 (доп)/Program.cs
@@ -3,14 +3,11 @@
     internal class Program
     {
         static void Main(string[] args)
-        { //использовать бесконечный цикл. Пользователь вводит название услуги ему выдается цена данной услуги. 
-            // при пустом значении цикл завершается. В конце вывести общую сумму услуг
-            Console.WriteLine("Введите название услуги");
-            string nameServer = Console.ReadLine();
-            for(; ;)
+        {
             {
-                Console.WriteLine("Всё для вас, всё бесплатно");
+
             }
+               
         }
     }
 }
---
+++ PZ_05/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Числовая последовательность Фибоначчи");
+            Console.Write("Введите число A, при условии, что A > 1: ");
+            int A = int.Parse(Console.ReadLine());
+
+            int b = 1;
+            int c = 1;
+            int count = 2;
+
+            while (c < A)
+            {
+                int temp = c;
+                c = c + b;
+                b = temp;
+                count = count + 1;
+            }
+
+            if (c == A)
+            {
+                Console.WriteLine($"Данное число является {count}-ым числом Фибоначчи");
+            }
+            else
+            {
+                Console.WriteLine("-1");
+            }
+
+
+        }
+               
+
+        }
+    }
