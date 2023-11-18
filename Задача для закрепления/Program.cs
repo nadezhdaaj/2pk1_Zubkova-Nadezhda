f1e69971b3f7299e19aaa7e878687d4429b18b27
Надежда Зубкова <nad875q@gmail.com>
2023-11-18 18:03:43 +0500

    задача для закрепления

---
+++ Задача для закрепления/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Задача_для_закрепления
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст: ");
+            string a = Console.ReadLine();
+            Regex s = new Regex(@"\b\d{ 4 }\b");
+            string result = s.Replace(a, "****");
+            Console.WriteLine("Изменённый текст: ");
+            }
+
+
+        }
+    }
+}
