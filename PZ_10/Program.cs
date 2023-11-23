d8c3855d1a8b5c2eb1781b35b717c9d67f876d68
Надежда Зубкова <nad875q@gmail.com>
2023-11-23 18:28:29 +0500

    Добавлена практика 10

---
+++ PZ_10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            string text = Console.ReadLine();
+            int count = 0;
+                foreach (string word in text.Split(' ')); //разбираем предложение на слова, используя пробел
+                {
+                if (word[0] == 'о' && word[word.Lenght() - 1] == 'о') //проверяем каждое слово, заканчивается и начинается  ли оно на "о"
+                {
+                    count++;   // если да, увеличиваем счётчик
+                }
+                }
+                Console.WriteLine($"Количество слов, начинающихся и заканчивающих на о равно {count}");
+        }
+    }
+}
